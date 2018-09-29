642615b955035e805b0e345e30f8a2206af342c9
HelenYankovskaya <elenalotto@inbox.ru>
2018-09-29 23:43:03 +0200

    "First commit"

---
+++ src/seven/Seven7.java
@@ -0,0 +1,28 @@
+package seven;
+
+public class Seven7 {
+
+    static int a = 27;
+    static double s;
+
+
+    public static void main(String[] args) {
+        s = a % 10;
+        if (s == 7) {
+            {
+                System.out.println("Последняя цифра числа равнa 7");
+            }
+        } else {
+            System.out.println("Последняя цифра числа не равна 7!!!!,а равна " + s);
+        }
+    }
+
+
+
+
+
+
+
+
+
+}
